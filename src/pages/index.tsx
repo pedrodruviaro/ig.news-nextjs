a2238f5b0bdedd76cb592935d17d8584b8c4d3af
pedrodruviaro <pedrodruviaro@gmail.com>
2021-11-04 08:57:55 -0300

    definindo estilos globais e configurando document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
-    return <h1>hello world</h1>;
+    return (
+        <>
+            <Head>
+                <title>Início | ig.news</title>
+            </Head>
+            <h1>hello world</h1>;
+        </>
+    );
 }
